5a47dd4bc5536f8e031e11b83848d049f5213661
Carlos V <iamthekook111@gmail.com>
2023-11-28 22:50:27 -0800

    revert changes regarding loading screen, back to the drawing board

--- script.js
+++ script.js
@@ -1,17 +1,3 @@
 function navigateTo(page) {
     window.location.href = page;
 }
-
-// SIMULATE LOADING
-setTimeout(function() {
-    // HIDE LOADER
-    document.getElementById('loading-section').style.display = 'none';
-    // SHOW MAIN CONTENT
-    document.body.style.visibility = 'visibile';
-}, 2000);
-
-function navigateTo(page) {
-    // SHOW LOADER BEFORE NAVIGATING TO MAIN CONTENT
-    document.getElementById('loading-section').style.display = 'block';
-    window.location.href = page;
-}
