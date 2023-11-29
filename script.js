dd734dcc7e6ce2ccd58c1017f4fd1740841f6db3
Carlos V <iamthekook111@gmail.com>
2023-11-28 18:13:50 -0800

    add functionality and loading js feature to html file

--- script.js
+++ script.js
@@ -2,3 +2,10 @@ function navigateTo(page) {
     window.location.href = page;
 }
 
+// SIMULATE LOADING
+setTimeout(function) {
+    //HIDE LOADER
+    document.getElementById('loader-wrapper').style.display = 'none';
+    //SHOW MAIN CONTENT
+    document.getElementById('content').style.display = 'block';
+}, 2000);
