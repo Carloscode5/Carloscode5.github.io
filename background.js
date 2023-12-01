e192ad20a8bc8bf12c80d7277f61dcc767aaf5fa
Carlos V <iamthekook111@gmail.com>
2023-11-30 16:22:30 -0800

    add background.js

---
+++ background.js
@@ -0,0 +1,15 @@
+// background.js
+
+chrome.runtime.onInstalled.addListener(() => {
+	console.log('Extension installed.');
+});
+
+chrome.runtime.onStartup.addListener(() => {
+	console.log('Extension started.');
+});
+
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+	if (message.action === 'fetchData') {
+	  // Call your fetchData function here or send a message to the content script to initiate the fetch
+	}
+});
