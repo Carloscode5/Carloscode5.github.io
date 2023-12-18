122093d9aae3de63be4dfa30cceb38bf86c73441
Carlos V <iamthekook111@gmail.com>
2023-12-17 22:31:33 -0800

    adding update to projects tab

--- background.js
+++ background.js
@@ -1,15 +1,15 @@
 // background.js
 
 chrome.runtime.onInstalled.addListener(() => {
-	console.log('Extension installed.');
+    console.log('Extension installed.');
 });
 
 chrome.runtime.onStartup.addListener(() => {
-	console.log('Extension started.');
+    console.log('Extension started.');
 });
 
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-	if (message.action === 'fetchData') {
-	  // Call your fetchData function here or send a message to the content script to initiate the fetch
-	}
+    if (message.action === 'fetchData') {
+        // Call your fetchData function here or send a message to the content script to initiate the fetch
+    }
 });
