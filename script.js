61ad8a29b69fcd1243eb9f60da3a1a01cad18f00
Carlos V <iamthekook111@gmail.com>
2023-12-22 17:20:48 -0800

    Add style to thumbnails

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
+// Use window.onload to confirm resources are loaded before hiding loading screen
+window.onload = function() {
+  // Hide loading screen
+  var loadingScreen = document.getElementById('loading-screen');
+  if (loadingScreen) {
+    loadingScreen.style.display = 'none';
+  }
+};
+
+// Function to navigate to a new page
 function navigateTo(page) {
-    window.location.href = page;
+  window.location.href = page;
 }
