88cfbe2191ba1b06be0c339c363f9259db044093
Carlos V <iamthekook111@gmail.com>
2023-12-22 21:16:08 -0800

    create preload function for website

--- script.js
+++ script.js
@@ -1,13 +1,25 @@
-// Use window.onload to confirm resources are loaded before hiding loading screen
-window.onload = function() {
-  // Hide loading screen
-  var loadingScreen = document.getElementById('loading-screen');
-  if (loadingScreen) {
-    loadingScreen.style.display = 'none';
-  }
-};
+// Check if the page has been loaded before
+const pageLoadedBefore = sessionStorage.getItem("pageLoadedBefore");
+
+// If the page has not been loaded before, show the preloader
+if (!pageLoadedBefore) {
+    document.addEventListener("DOMContentLoaded", function () {
+        // Fade out the preloader
+        setTimeout(function () {
+            document.getElementById("preloader").style.opacity = "0";
+            document.getElementById("content").style.display = "block";
+        }, 2000); // You can adjust the delay time as needed
+
+        // Set a flag in sessionStorage to indicate that the page has been loaded
+        sessionStorage.setItem("pageLoadedBefore", true);
+    });
+} else {
+    // If the page has been loaded before, simply display the content
+    document.getElementById("preloader").style.display = "none";
+    document.getElementById("content").style.display = "block";
+}
 
 // Function to navigate to a new page
 function navigateTo(page) {
-  window.location.href = page;
+    window.location.href = page;
 }
